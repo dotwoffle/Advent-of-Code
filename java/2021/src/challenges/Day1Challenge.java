666069070aa941fb3781674789d30fe90bbe9bda
Gbagecol <54816222+Gbagecol@users.noreply.github.com>
2021-12-16 23:28:27 -0700

    Java 2021/1

--- java/2021/src/challenges/Day1Challenge.java
+++ java/2021/src/challenges/Day1Challenge.java
@@ -2,9 +2,19 @@ package challenges;
 
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.util.ArrayList;
 
 
+/**Day 1 challenge: <a href="https://adventofcode.com/2021/day/1">Sonar Sweep</a>*/
 public class Day1Challenge extends Challenge {
+	
+	/*******************/
+	/*    Variables    */
+	/*******************/
+	
+	/**The depth measurements as integers.*/
+	private ArrayList<Integer> measurements;
+	
 
 	/*********************/
 	/*    Constructor    */
@@ -14,7 +24,14 @@ public class Day1Challenge extends Challenge {
 	 * @throws FileNotFoundException If the input file for this challenge is not found.
 	 * @throws IOException*/
 	public Day1Challenge() throws FileNotFoundException, IOException {
+		
 		super(2021, 1);
+		
+		this.measurements = new ArrayList<Integer>();
+		
+		for(String line : input)
+			measurements.add(Integer.parseInt(line));
+		
 	}
 	
 	
@@ -24,14 +41,37 @@ public class Day1Challenge extends Challenge {
 
 	@Override
 	protected void challengePart1() {
+		
+		int numIncreases = 0; //total measurement increases
 
-		System.out.println(input);
+		//find increasing measurements
+		for(int x = 1; x < measurements.size(); x++)
+			if(measurements.get(x) > measurements.get(x-1))
+				numIncreases++;
+		
+		System.out.println(numIncreases);
 		
 	}
 
 	@Override
 	protected void challengePart2() {
-		// TODO Auto-generated method stub
+		
+		int numIncreases = 0; //total measurement increases
+		
+		//loop through measurements
+		for(int x = 3; x < measurements.size(); x++) {
+			
+			//sum groups of 3 measurements
+			int prevGroupSum = measurements.get(x-3) + measurements.get(x-2) + measurements.get(x-1);
+			int newGroupSum = measurements.get(x-2) + measurements.get(x-1) + measurements.get(x);
+			
+			//check if new sum is greater than older
+			if(newGroupSum > prevGroupSum)
+				numIncreases++;
+			
+		}
+		
+		System.out.println(numIncreases);
 
 	}
 
