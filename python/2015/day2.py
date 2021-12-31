69bac99eedc937ea35994f7a2270faa4eb7f4e49
Gbagecol <54816222+Gbagecol@users.noreply.github.com>
2021-12-31 11:17:35 -0700

    Python 2015/2 Part 2

--- python/2015/day2.py
+++ python/2015/day2.py
@@ -29,12 +29,24 @@ def challengePart1(input):
         length, width, height = [int(dim) for dim in line.split("x")] #get dimensions
         sides = [length * width, length * height, width * height] #side areas
 
-        totalPaper += 2 * sides[0] + 2 * sides[1] + 2 * sides[2] + min(sides) #calculate total paper for thsi box
+        totalPaper += 2 * sides[0] + 2 * sides[1] + 2 * sides[2] + min(sides) #calculate total paper for this box
 
     print(totalPaper)
 
 def challengePart2(input):
-    pass
+    
+    totalRibbon = 0 #total feet of ribbon
+
+    #add ribbon totals
+    for line in input:
+
+        dimensions = [int(dim) for dim in line.split("x")] #get dimensions
+        dimensions.sort() #sort dimensions
+
+        #calculate ribbon
+        totalRibbon += 2 * dimensions[0] + 2 * dimensions[1] + (dimensions[0] * dimensions[1] * dimensions[2])
+
+    print(totalRibbon)
 
 
 ##############
