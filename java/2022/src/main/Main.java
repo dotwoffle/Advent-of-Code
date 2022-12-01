5f1f7f9b0b2f2e7eaebee9f8ee1b0f84c8701650
Gbagecol <54816222+Gbagecol@users.noreply.github.com>
2022-12-01 12:09:08 -0700

    Setup for java 2022

---
+++ java/2022/src/main/Main.java
@@ -0,0 +1,28 @@
+package main;
+
+import challenges.Challenge;
+//import challenges.Day1Challenge;
+
+
+/**Driver class for challenges.*/
+public class Main {
+
+	/**************/
+	/*    Main    */
+	/**************/
+	
+	public static void main(String[] args) {
+		
+		try {
+			
+			//Challenge challenge = new Day1Challenge();
+			
+			//challenge.runChallenge();
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+	}
+
+}
